d4399a222f13215d7afd1ab45114d9157564f700
Jouni Juvonen <ejoju004@edu.xamk.fi>
2024-01-29 09:29:55 +0200

    created root route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 import './index.css'
 
+// defined root route
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>
+  }
+])
+
+// root route
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
