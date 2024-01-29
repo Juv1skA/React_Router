4b6d79a1164686ff9c78fd98e0a1500a0e174c6b
Jouni Juvonen <ejoju004@edu.xamk.fi>
2024-01-29 09:46:04 +0200

    added contact route

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,9 @@ import {
   RouterProvider,
 } from "react-router-dom"
 import './index.css'
-import Root from "./routes/root" // root layout
+import Root from "./routes/root" // root layout component
 import ErrorPage from './error-page' // error page
+import Contact from './routes/contact' // contact component
 
 // defined root route
 const router = createBrowserRouter([
@@ -14,6 +15,12 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  
+  // contact route inside root route
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   }
 ])
 
